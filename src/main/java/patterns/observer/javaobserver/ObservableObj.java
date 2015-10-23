4d39215a1983b3feb5e6573f803d974a53c0e6aa
Edgar <edjuradob@gmail.com>
2015-10-23 09:21:24 -0500

    Changes in observer

--- src/main/java/patterns/observer/javaobserver/ObservableObj.java
+++ src/main/java/patterns/observer/javaobserver/ObservableObj.java
@@ -3,12 +3,7 @@
  */
 package patterns.observer.javaobserver;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
-import java.util.Observer;
-
-import patterns.observer.interfaces.ObserverInterface;
 
 /**
  * @author ejurado
@@ -16,29 +11,7 @@ import patterns.observer.interfaces.ObserverInterface;
  */
 public class ObservableObj extends Observable{
 
-	List<Observer> lstObservers;
-	boolean hasChanged;
 	String state;
-	
-	public ObservableObj(){
-		lstObservers=new ArrayList<Observer>();
-		hasChanged=false;
-	}
-	
-	protected void setChanged(){
-		hasChanged=true;
-	}
-	
-	public void addObserver(Observer o){
-		lstObservers.add(o);
-	}
-	
-	public void notifyObservers(){
-		for (Observer observer : lstObservers) {
-			observer.update(this, null);
-		}
-		hasChanged=false;
-	}
 	
 	public void setState(String newState){
 		state=newState;
